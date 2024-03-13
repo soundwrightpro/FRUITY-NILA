1d1b7adbe2b189c59c96dbd6c73abba3143ecc48
Duwayne <64506989+soundwrightpro@users.noreply.github.com>
2024-03-13 12:51:33 -0400

    version number update

--- Native Instruments/script/device_setup/constants.py
+++ Native Instruments/script/device_setup/constants.py
@@ -7,7 +7,7 @@ other constants for MIDI script functionality.
 """
 
 # Version information
-VERSION_NUMBER = "v14.0.3"
+VERSION_NUMBER = "v14.0.5"
 HELLO_MESSAGE = "FRUITY NILA"
 GOODBYE_MESSAGE = "by: Duwayne"
 OUTPUT_MESSAGE = f"\nFRUITY NILA {VERSION_NUMBER}\nCopyright © 2023 Duwayne\n"
