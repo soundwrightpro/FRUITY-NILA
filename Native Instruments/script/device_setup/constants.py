c4de2c35ad8f8f8d69bda51f68d3cb005c1b5e31
Duwayne <64506989+soundwrightpro@users.noreply.github.com>
2025-07-01 15:55:30 -0400

    updated version number

--- Native Instruments/script/device_setup/constants.py
+++ Native Instruments/script/device_setup/constants.py
@@ -7,7 +7,7 @@ other constants for MIDI script functionality.
 """
 
 # Version information
-VERSION_NUMBER = "v2024.1"
+VERSION_NUMBER = "v2024.1.1"
 HELLO_MESSAGE = "FRUITY NILA"
 GOODBYE_MESSAGE = "by: sound"
 OUTPUT_MESSAGE = f"\nFRUITY NILA {VERSION_NUMBER}\n"
